e27953177961f010f6485ac08e5b4d626bbf5a18
Taavi1p <42722494+Taavi1p@users.noreply.github.com>
2021-12-07 11:02:15 +0200

    Add files via upload

---
+++ rekursiivne.java
@@ -0,0 +1,16 @@
+public class rekursiivne {
+
+    public static String remover(String word) {
+
+    }
+    
+    public static boolean vowel(String täht) {
+     if  (täht.equals())
+    }
+
+    public static void main(String[] args) {
+
+        String sõna = "telefon";
+        System.out.println(remover(sõna));
+    }
+}
